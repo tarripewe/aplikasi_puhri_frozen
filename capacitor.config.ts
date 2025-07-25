ed32d489a99634738521ca4cf21f590695c71aa5
“tarripewe” <tarri@karadigital.co>
2025-07-25 20:42:12 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aplikasi_Puhri_Frozen',
+  webDir: 'www'
+};
+
+export default config;
