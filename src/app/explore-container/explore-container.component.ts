4e940fb62c991b10a79d0ea44b5c1e4d93ebdf27
“tarripewe” <tarri@karadigital.co>
2025-07-26 10:51:03 +0700

    UI Produk

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
   standalone: false,
 })
+
 export class ExploreContainerComponent {
+  @Input() name!: string;
+  @Input() harga!: number;
+  @Input() uom!: string;
+  @Input() barcode!: string;
+  @Input() stock!: string;
+  @Input() qty!: number;
 
-  @Input() name?: string;
-
+  formatRupiah(angka: number): string {
+    return 'Rp ' + angka.toLocaleString('id-ID');
+  }
 }
+
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  selector: 'app-tab1', // ✔ Nama selector sesuai
+  templateUrl: 'tab1.page.html', // ✔ Mengarah ke file template yang benar
+  styleUrls: ['tab1.page.scss'], // ✔ Gaya SCSS terhubung
+  standalone: false, // ✔ 'false' jika bagian dari module (misalnya TabsPageModule)
 })
 export class Tab1Page {
+  products = [
+    {
+      name: 'Sosis So Nice',
+      harga: 12000,
+      uom: 'pak',
+      barcode: '265493564554647',
+      stock: 'Out of stock',
+      qty: 200,
+    },
+    {
+      name: 'Nugget Fiesta',
+      harga: 18000,
+      uom: 'pak',
+      barcode: '1234567890123',
+      stock: 'In stock',
+      qty: 200,
+    },
+    {
+      name: 'Chicken Stick',
+      harga: 15000,
+      uom: 'pak',
+      barcode: '9876543210987',
+      stock: 'In stock',
+      qty: 200,
+    },
+    {
+      name: 'Bakso Sapi',
+      harga: 14000,
+      uom: 'pak',
+      barcode: '3213213213210',
+      stock: 'Out of stock',
+      qty: 200,
+    },
+    {
+      name: 'Dimsum Ayam',
+      harga: 13000,
+      uom: 'pak',
+      barcode: '1112223334445',
+      stock: 'Re stock',
+      qty: 200,
+    },
+  ];
 
-  constructor() {}
-
+  constructor() {} // ✔ Konstruktor boleh kosong jika tidak ada dependency
 }
